51bba44333ad50dda173b43afec79a6693305b80
Ingrid Fernanda <100444234+IngridFBalbino@users.noreply.github.com>
2023-06-15 08:42:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+
+function escrevendoLetra(){
+    function ativaLetra(elemento){
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML  = '';
+        arrTexto.forEach((letra, i)=>{
+            setTimeout(()=>{
+                elemento.innerHTML += letra; 
+            }, 75 * i);
+        });
+    }
+
+    const titulo = document.querySelector('.digitando');
+    ativaLetra(titulo);
+} 
+
+
+
+const listaALL = document.querySelectorAll('.projects_armazenamento ul li');
+const buttonGeral = document.querySelectorAll('.project_navegacao li');
+const buttonALL = document.querySelector('.project_models .all');
+
+listaALL.forEach((item)=>{
+    item.classList.add('ativo');
+})
+
+function removeClick(index){
+    buttonGeral.forEach((item)=>{
+        item.classList.remove('ativo');
+    })
+    buttonGeral[index].classList.add('ativo')
+}
+
+buttonGeral.forEach((event,index)=>{
+    event.addEventListener('click', ()=>{
+        removeClick(index)
+    })
+})
+
+function showLista(lista, buttom = "all"){
+    lista.forEach((item)=>{
+        item.classList.remove('ativo');
+    });
+
+    if(buttom == 'design'){
+        lista[0].classList.add('ativo')
+        lista[1].classList.add('ativo')
+    }
+    if(buttom == 'graphic'){
+        lista[2].classList.add('ativo');
+        lista[3].classList.add('ativo');
+    }
+
+    if(buttom == 'website'){
+        lista[4].classList.add('ativo');
+        lista[5].classList.add('ativo');
+        lista[6].classList.add('ativo');
+        lista[7].classList.add('ativo');
+    }
+
+    if(buttom == 'all'){
+        lista[0].classList.add('ativo')
+        lista[1].classList.add('ativo')
+        lista[2].classList.add('ativo');
+        lista[3].classList.add('ativo');
+        lista[4].classList.add('ativo');
+        lista[5].classList.add('ativo');
+        lista[6].classList.add('ativo');
+        lista[7].classList.add('ativo');
+    }
+}
+
+buttonGeral.forEach((item)=>{
+    item.addEventListener('click', (e)=>{
+        let currentButton = e.target;
+        if(currentButton.classList.contains('all')){
+            showLista(listaALL);
+        } if(currentButton.classList.contains('design')){
+            showLista(listaALL, "design")
+        }
+
+        if(currentButton.classList.contains('graphic')){
+            showLista(listaALL, "graphic")
+        }
+
+        if(currentButton.classList.contains('website')){
+            showLista(listaALL, "website")
+        }
+
+        if(currentButton.classList.contains('all')){
+            showLista(listaALL, "all")
+        }
+    });
+});
+
+
+'use strict';
+
+function typeWriter(el) {
+    const textArray = el.innerHTML.split('');
+    el.innerHTML = '';
+    textArray.forEach((letter, i) =>
+        setTimeout(() => (el.innerHTML += letter), 95 * i)
+    );
+
+    setInterval(() => typeWriter(el), 8000);
+}
+
+typeWriter(elementEl);
